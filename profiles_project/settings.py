837aaf1cbcabd12c7f7d06813884c7dfefc048b1
Paul Gasbarra <paulgasbarra@gmail.com>
2022-05-24 12:29:04 -0400

    update files

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x8w*k-_j$6h&8+eelmj&n0a%^d5s^(3*i-rok9kpw5w5^68iq^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
@@ -126,3 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT='static/'
